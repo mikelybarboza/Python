455f412dd591f310b99996011f8f3e675ff2eaba
Mikely Barboza <barbozamikely@gmail.com>
2024-01-02 16:43:06 -0300

    aula6

--- CursoPython3/aula6.py
+++ CursoPython3/aula6.py
@@ -4,3 +4,12 @@
 # tipos imutáveis e primitivos:
 # str, int, float, bool
 
+#print('1' + 1)
+print('a' + 'b')
+print('1', type('1'))
+print(int('1'), type(int('1')))
+print(int('1') + 1)
+print((float('1') + 1))
+print((type(float('1') + 1)))
+print(bool(''))
+print(bool(' '))
